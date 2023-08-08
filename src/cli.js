522278780f60ebd272717e3b773d35373ffae061
Pashkova Daria <dar.pashcovaa@gmail.com>
2023-08-08 14:56:29 +0900

    fix bug in brain-games section

--- src/cli.js
+++ src/cli.js
@@ -3,5 +3,5 @@ import readlineSync from 'readline-sync';
 export const welcomeUser = () => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello ${userName}`);
+  console.log(`Hello, ${userName}`);
 };
