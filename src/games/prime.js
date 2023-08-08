aac3051359b7374ff37f4d43c09ac86a57cc41e6
Pashkova Daria <dar.pashcovaa@gmail.com>
2023-08-08 16:30:50 +0900

    update prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -4,7 +4,7 @@ import getRandomNumInRange from '../randomNumber.js';
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (num) => {
-  if (num <= 1) {
+  if (num < 2) {
     return 'yes';
   }
   for (let i = 2; i < Math.sqrt(num); i += 1) {
